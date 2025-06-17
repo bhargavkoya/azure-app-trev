68e09e20344f69fbb10f8b001a2707a259959a9b
bhargav koya <bhargavkoya99@gmail.com>
2025-06-17 14:31:30 +0530

    Add project files.

---
+++ azure-app-trev/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace azure_app_trev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
